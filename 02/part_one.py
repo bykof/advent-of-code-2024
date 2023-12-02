afbe541f1bad18a346e9f82af90877766f68cec9
Michael Bykovski <bykof@me.com>
2023-12-02 16:10:07 +0100

    day 2

---
+++ 02/part_one.py
@@ -0,0 +1,77 @@
+from dataclasses import dataclass
+import enum
+import typing
+
+
+class Color(str, enum.Enum):
+    red = "red"
+    green = "green"
+    blue = "blue"
+
+
+@dataclass
+class GameRound:
+    store: dict[Color, int]
+
+    @property
+    def is_fitting(self):
+        return (
+            self.store[Color.green] <= 13
+            and self.store[Color.red] <= 12
+            and self.store[Color.blue] <= 14
+        )
+
+
+@dataclass
+class Game:
+    round: int
+    game_rounds: typing.List[GameRound]
+
+    @property
+    def is_win(self):
+        for game_round in self.game_rounds:
+            if not game_round.is_fitting:
+                return False
+        return True
+
+
+# Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+def read_game(line: str) -> Game:
+    game_meta, rounds = line.split(":")
+
+    game_rounds = []
+    for round in rounds.split(";"):
+        store = {color.value: 0 for color in Color}
+        color_counts = round.split(",")
+        for color_count in color_counts:
+            count, color = color_count.strip().split(" ")
+            store[color] += int(count)
+        game_rounds.append(
+            GameRound(
+                store=store,
+            )
+        )
+    return Game(round=int(game_meta.replace("Game ", "")), game_rounds=game_rounds)
+
+
+def read_games(input_lines: typing.List[str]) -> typing.List[Game]:
+    return [read_game(input_line) for input_line in input_lines]
+
+
+def sum_of_possible_game_ids(games: typing.List[Game]) -> int:
+    return sum([game.round for game in games if game.is_win])
+
+
+def read_input_lines(filepath: str) -> typing.List[str]:
+    with open(filepath, "r") as input_file:
+        return input_file.readlines()
+
+
+def main():
+    input_lines = read_input_lines("./part_one_input")
+    game_rounds = read_games(input_lines=input_lines)
+    result = sum_of_possible_game_ids(games=game_rounds)
+    print(f"Result is: {result}")
+
+
+main()
---
+++ 02/test_part_two.py
@@ -0,0 +1,105 @@
+from part_two import Color, Game, GameRound, read_game, sum_of_power
+
+
+def test_game_round():
+    assert Game(
+        round=1,
+        game_rounds=[
+            GameRound(store={Color.red: 4, Color.green: 0, Color.blue: 3}),
+            GameRound(store={Color.red: 1, Color.green: 2, Color.blue: 6}),
+            GameRound(store={Color.red: 0, Color.green: 2, Color.blue: 0}),
+        ],
+    ) == read_game("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+    assert Game(
+        round=4,
+        game_rounds=[
+            GameRound(store={Color.red: 3, Color.green: 1, Color.blue: 6}),
+            GameRound(store={Color.red: 6, Color.green: 3, Color.blue: 0}),
+            GameRound(store={Color.red: 14, Color.green: 3, Color.blue: 15}),
+        ],
+    ) == read_game(
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+    )
+
+
+def assert_is_fitting():
+    assert (
+        GameRound(store={Color.red: 3, Color.green: 1, Color.blue: 6}).is_fitting
+        == True
+    )
+    assert (
+        GameRound(store={Color.red: 13, Color.green: 1, Color.blue: 6}).is_fitting
+        == False
+    )
+    assert (
+        GameRound(store={Color.red: 2, Color.green: 14, Color.blue: 6}).is_fitting
+        == False
+    )
+    assert (
+        GameRound(store={Color.red: 2, Color.green: 2, Color.blue: 15}).is_fitting
+        == False
+    )
+
+
+def assert_is_win():
+    assert (
+        Game(
+            round=4,
+            game_rounds=[
+                GameRound(store={Color.red: 3, Color.green: 1, Color.blue: 6}),
+                GameRound(store={Color.red: 6, Color.green: 3, Color.blue: 0}),
+                GameRound(store={Color.red: 14, Color.green: 3, Color.blue: 15}),
+            ],
+        ).is_win
+        == False
+    )
+
+    assert (
+        Game(
+            round=4,
+            game_rounds=[
+                GameRound(store={Color.red: 3, Color.green: 1, Color.blue: 6}),
+                GameRound(store={Color.red: 6, Color.green: 3, Color.blue: 0}),
+                GameRound(store={Color.red: 12, Color.green: 3, Color.blue: 14}),
+            ],
+        ).is_win
+        == True
+    )
+
+
+def test_sum_of_possible_game_ids():
+    games = [
+        Game(
+            round=2,
+            game_rounds=[
+                GameRound(store={Color.red: 23, Color.green: 7, Color.blue: 21}),
+            ],
+        ),
+        Game(
+            round=1,
+            game_rounds=[
+                GameRound(store={Color.red: 5, Color.green: 4, Color.blue: 9}),
+                GameRound(store={Color.red: 1, Color.green: 3, Color.blue: 7}),
+            ],
+        ),
+        Game(
+            round=4,
+            game_rounds=[
+                GameRound(store={Color.red: 5, Color.green: 4, Color.blue: 9}),
+                GameRound(store={Color.red: 1, Color.green: 3, Color.blue: 7}),
+            ],
+        ),
+    ]
+
+    assert sum_of_power(games=games) == 5
+
+
+def test_power_of_maximum_colors():
+    game = Game(
+        round=4,
+        game_rounds=[
+            GameRound(store={Color.red: 5, Color.green: 4, Color.blue: 9}),
+            GameRound(store={Color.red: 1, Color.green: 5, Color.blue: 7}),
+        ],
+    )
+    assert game.power_of_maximum_colors() == 5 * 5 * 9
